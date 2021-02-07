f6184d5b867e2a2a53ab6986770d5fa063ad4e5e
jspearus <35698087+jspearus@users.noreply.github.com>
2021-02-07 16:47:05 -0600

    uart on nano working!!

--- Py_Scripts/Rover.py
+++ Py_Scripts/Rover.py
@@ -3,7 +3,7 @@ from rover_control import RoverControl
 rc = RoverControl()
 
 print("Rover Terminal Control")
-print("Can You See Me?")
+
 
 
 rc.Serial_out()
--- Py_Scripts/rover_control.py
+++ Py_Scripts/rover_control.py
@@ -1,6 +1,15 @@
 import time
 import serial
-arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+
+arduino = serial.Serial(
+    port="/dev/ttyTHS1",
+    baudrate=9600,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+)
+
+# arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 class RoverControl():
   def __init__(self):
@@ -15,12 +24,14 @@ class RoverControl():
      while True:
         Throttle = input('Enter Throttle? -100 to 100 ')
         if Throttle == 'q':
-          print("Script Finished")
+          arduino.close()
+          print("Serial Com Terminated")
           break
         Steering = input('Enter Steering? -100 to 100 ')
-        arduino.write(bytes('move@','utf-8') + bytes(Throttle,'utf-8') + bytes('-', 'utf-8') + bytes(Steering,'utf-8') + bytes('#\n','utf-8')) 
+        #arduino.write(bytes('move@','utf-8') + bytes(Throttle,'utf-8') + bytes('$', 'utf-8') + bytes(Steering,'utf-8') + bytes('#\n','utf-8'))
+        arduino.write(bytes('move@','utf-8') + bytes(Throttle,'utf-8') + bytes('$', 'utf-8') + bytes(Steering,'utf-8') + bytes('#\n','utf-8'))  
         time.sleep(1)
-        arduino.write(bytes('move@','utf-8') + bytes('0', 'utf-8') + bytes('-', 'utf-8') + bytes('0','utf-8') + bytes('#\n','utf-8')) 
+        arduino.write(bytes('move@','utf-8') + bytes('0', 'utf-8') + bytes('$', 'utf-8') + bytes('0','utf-8') + bytes('#\n','utf-8')) 
         print("rover Stopped")
 
 
