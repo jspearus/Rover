6042b8d8acfc2e950970c481341952a0718710dd
jspearus <35698087+jspearus@users.noreply.github.com>
2021-02-22 10:50:31 -0600

    working on lidar

--- Py_Scripts/Rover.py
+++ Py_Scripts/Rover.py
@@ -1,24 +1,14 @@
-#from rover_control import RoverControl
-from lidar_Nav import lidarNav
+from rover_control import RoverControl
+#from lidar_Nav import lidarNav
 
-<<<<<<< HEAD
 
 rc = RoverControl()
 # ln = lidarNav()
-=======
-#rc = RoverControl()
-ln = lidarNav()
->>>>>>> 41ac275c74c8eaa3e1b42f5934d76a9fb0d40de1
 
 print("Rover Terminal Control")
 
 
 
-<<<<<<< HEAD
 rc.Serial_out()
 # ln.get_info()
 # ln.stopLidar()
-=======
-#rc.Serial_out()
-ln.getInfo()
->>>>>>> 41ac275c74c8eaa3e1b42f5934d76a9fb0d40de1
