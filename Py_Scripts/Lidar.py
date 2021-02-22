af3b56d420c4875ccf02cd52b9396845083fe8fc
jspearus <35698087+jspearus@users.noreply.github.com>
2021-02-22 10:45:36 -0600

    working on lidar

---
+++ Py_Scripts/Lidar.py
@@ -0,0 +1,20 @@
+
+
+
+
+class lidarNav():
+    def __init__(self):
+        print("Lidar Initialized")  
+
+    def get_info(self):
+       
+
+
+    def stopLidar(self):
+        lidar.connect()
+        lidar.stop_motor() 
+
+
+if __name__ =='__main__':
+    print("Lidar Navigation class")
+    print("run form import")
--- Py_Scripts/Rover.py
+++ Py_Scripts/Rover.py
@@ -1,9 +1,13 @@
 from rover_control import RoverControl
 
+
 rc = RoverControl()
+# ln = lidarNav()
 
 print("Rover Terminal Control")
 
 
 
 rc.Serial_out()
+# ln.get_info()
+# ln.stopLidar()
