edb7b8a8b90f23ae1d27bd86d1181345b5281d61
jspearus <35698087+jspearus@users.noreply.github.com>
2021-03-12 09:57:36 -0600

    moved control loop to main script

--- Py_Scripts/Rover.py
+++ Py_Scripts/Rover.py
@@ -11,6 +11,11 @@ print("Rover Terminal Control")
 
 
 #rc.Serial_out()
+Throttle = input("Enter Throttle: ")
+Steering = input("Enter Steering: ")
+rc.move_Rover(Throttle, Steering)
+time.sleep(1)
+rc.move_Rover(0,0)
 ln.startLidar()
 time.sleep(5)
 ln.stopLidar()
